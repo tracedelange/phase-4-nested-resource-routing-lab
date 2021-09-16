de90b001f6b7a8a971d8ea2d0eee3ae93b9b50b1
Trace <tracedelange@me.com>
2021-09-16 15:00:51 -0500

    commit tree dfe2876ba1cbd7633d79e3b34b2875370bab9f41

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items
+
   end
 
 end
+
+if params[:dog_house_id]
+  dog_house = DogHouse.find(params[:dog_house_id])
+  reviews = dog_house.reviews
+else
+  reviews = Review.all
+end
+render json: reviews, include: :dog_house
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index] do
+
+
+  end
+
+  resources :users, only: [:show] do
+    resources :items, only: [:index, :show]
+
+  end
 end
